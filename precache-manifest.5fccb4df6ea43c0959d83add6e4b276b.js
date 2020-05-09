3b8e9458fa639b2730a116114dc72773c3444b18
muhammedfajer <muhammedfajer12@gmail.com>
2020-05-09 23:37:51 +0530

    Updates

---
+++ precache-manifest.5fccb4df6ea43c0959d83add6e4b276b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5febf7a74956beeb4f7cf505fad420e",
+    "url": "/c19Info/index.html"
+  },
+  {
+    "revision": "597c7a15521213ab3771",
+    "url": "/c19Info/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "801c3cd03323d00a88e2",
+    "url": "/c19Info/static/css/main.2791a3c3.chunk.css"
+  },
+  {
+    "revision": "597c7a15521213ab3771",
+    "url": "/c19Info/static/js/2.50b5ed43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/c19Info/static/js/2.50b5ed43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801c3cd03323d00a88e2",
+    "url": "/c19Info/static/js/main.29a693aa.chunk.js"
+  },
+  {
+    "revision": "0d4caf6c70376a2a950c",
+    "url": "/c19Info/static/js/runtime-main.a1158a31.js"
+  }
+]);
